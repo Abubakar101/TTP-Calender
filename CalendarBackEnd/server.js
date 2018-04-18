f9f40c6896271818fe5dfec94978f6509e227752
Muhammad Abubakar <muhammadabubakar0419@gmail.com>
2018-04-17 23:57:47 -0400

    setting up the server.js, package json and gitignore

---
+++ CalendarBackEnd/server.js
@@ -0,0 +1,42 @@
+/* setting up dependencies */
+const express = require("express");
+const logger = require("morgan");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+
+/* setting up port & listen */
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, function() {
+  console.log(`listening on port ${PORT}`);
+});
+
+/* setting up cors */
+app.use(cors());
+/* setting up logger */
+app.use(logger("dev"));
+
+/* setting up body parser */
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Express only serves static assets in production
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("../CalendarFrontEnd/build"));
+}
+
+// else if (process.env.NODE_ENV === "development") {
+//   app.get("/events", function(req, res) {
+//     res.json()
+//   });
+// }
+
+/* Events API route */
+const eventRoutes = require("./routes/eventroutes");
+app.use("/api/events", eventRoutes);
+
+/* handling 404 */
+app.get("*", function(req, res) {
+  res.status(404).send({ message: "Oops! Not found." });
+});
