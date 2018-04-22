01edf0e442ac466796881a2128273d1d62a35d83
Muhammad Abubakar <muhammadabubakar0419@gmail.com>
2018-04-22 16:36:24 -0400

    Update question_C.js

--- question_C.js
+++ question_C.js
@@ -8,15 +8,15 @@ This can be solved iteratively or recursively, either is fine.
 
 
 */
-function countSteps(n = 0, map = {}) {
+function countWays(n = 0, map = {}) {
 	if (n < 0) {
 		return 0;
 	} else if (n === 0) {
 		return 1;
 	} else if (map[n] === undefined) {
-	  map[n] = [1,2,3].reduce((a,c) =>  a+countSteps(n-c, map), 0) 
+	  map[n] = [1,2,3].reduce((a,c) =>  a+countWays(n-c, map), 0) 
 	}
 	return map[n];
 }
 	
-countSteps(20)
+countWays(20)
