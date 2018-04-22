04b9081eff9dd19045c2549b950e8e54276070fd
Muhammad Abubakar <muhammadabubakar0419@gmail.com>
2018-04-22 15:48:39 -0400

    Update question_C.js

--- question_C.js
+++ question_C.js
@@ -8,17 +8,15 @@ This can be solved iteratively or recursively, either is fine.
 
 
 */
-function countSteps(target) {
-	let steps = [1, 2, 3],
-		sums = [0],
-		count = 0;
-
-	while (sums.length) {
-		let currSum = sums.pop();
-		if (currSum < target) steps.forEach(n => sums.push(currSum + n));
-		if (currSum === target) count++;
+function countSteps(n = 0, map = {}) {
+	if (n < 0) {
+		return 0;
+	} else if (n === 0) {
+		return 1;
+	} else if (map[n] === undefined) {
+	  map[n] = [1,2,3].reduce((a,c) =>  a+countSteps(n-c, map), 0) 
 	}
-	return count;
+	return map[n];
 }
-
-countSteps(5);
+	
+countSteps(20)
